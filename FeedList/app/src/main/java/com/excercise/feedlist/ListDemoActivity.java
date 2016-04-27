775efca74f3b7aa5854cd27b44095fb0a68bcaa0
Kirubapradeep <kiruba777@gmail.com>
2016-04-27 19:10:05 +0530

    Refresh and Picasso changes
    Using Picasso for image download and caching

--- FeedList/app/src/main/java/com/excercise/feedlist/ListDemoActivity.java
+++ FeedList/app/src/main/java/com/excercise/feedlist/ListDemoActivity.java
@@ -1,8 +1,13 @@
 package com.excercise.feedlist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.excercise.feedlist.model.ListItem;
 import com.excercise.feedlist.presenter.ListDemoPresenter;
@@ -10,38 +15,86 @@ import com.excercise.feedlist.presenter.ListDemoPresenterImpl;
 
 import java.util.List;
 
-public class ListDemoActivity extends AppCompatActivity implements ListDemoView {
+import butterknife.ButterKnife;
+
+public class ListDemoActivity extends AppCompatActivity implements ListDemoView,
+        SwipeRefreshLayout.OnRefreshListener {
 
     private RecyclerView mListView;
-    private ListDemoPresenter mPresenter;
+    private RelativeLayout mRootView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private ListDemoPresenter mPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.setDebug(true);
+        ButterKnife.bind(this);
+
+        mListView = (RecyclerView) findViewById(R.id.demo_list);
+        mRootView = (RelativeLayout) findViewById(R.id.rootview);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.pull_to_refresh);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mListView.setLayoutManager(mLayoutManager);
+
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
         mPresenter = new ListDemoPresenterImpl(this);
+        //Load the Data
         mPresenter.requestData();
-    }
+        mSwipeRefreshLayout.setRefreshing(true);
+     }
+
+    /*
+     * Set Title for the View
+     */
 
     @Override
     public void setTitle(String title) {
-
+        setTitle((CharSequence) title);
     }
 
+    /*
+     * Populate the Data
+     */
     @Override
     public void populateData(List<ListItem> data) {
 
         if (getBaseContext() != null  && !isFinishing()) {
+            mSwipeRefreshLayout.setRefreshing(false);
             mAdapter = new ListDemoAdapter(data, this);
             mListView.setAdapter(mAdapter);
         }
+    }
 
+    /*
+     * Show Error in case of Failure
+     */
+    @Override
+    public void showError(String errorMessage) {
+        if (getBaseContext() != null  && !isFinishing()) {
+            mSwipeRefreshLayout.setRefreshing(false);
+            Snackbar.make(mRootView, errorMessage, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.retry, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mPresenter.requestData();
+                        }
+                    })
+                    .show();
+        }
     }
 
     @Override
-    public void showError(String errorMessage) {
-
+    public void onRefresh() {
+        mPresenter.requestData();
     }
 }
--- FeedList/app/src/main/java/com/excercise/feedlist/ListDemoAdapter.java
+++ FeedList/app/src/main/java/com/excercise/feedlist/ListDemoAdapter.java
@@ -2,11 +2,14 @@ package com.excercise.feedlist;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.excercise.feedlist.model.ListItem;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -16,33 +19,47 @@ import java.util.List;
 public class ListDemoAdapter extends RecyclerView.Adapter<ListDemoAdapter.ViewHolder> {
 
     private List<ListItem> mItems;
+    private Picasso mPicasso;
 
     public ListDemoAdapter(List<ListItem> listItems, Context context) {
         mItems = listItems;
+        mPicasso = Picasso.with(context);
     }
 
     @Override
-    public ListDemoAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_list_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ListDemoAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final ListItem item = mItems.get(position);
 
+        holder.title.setText(item.title);
+        holder.description.setText(item.description);
+        mPicasso.load(item.imageHref)
+                .resizeDimen(R.dimen.image_width, R.dimen.image_width)
+                .into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mItems.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView title;
         TextView description;
+        ImageView imageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
+            imageView = (ImageView) itemView.findViewById(R.id.image);
         }
     }
 }
