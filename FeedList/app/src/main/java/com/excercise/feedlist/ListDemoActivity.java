26aa5900299f5616f04f6c8b46c5f1a6da444489
Kirubapradeep <kiruba777@gmail.com>
2016-04-27 15:37:45 +0530

    Initial commit
    List application initial commit from IDE

---
+++ FeedList/app/src/main/java/com/excercise/feedlist/ListDemoActivity.java
@@ -0,0 +1,13 @@
+package com.excercise.feedlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
